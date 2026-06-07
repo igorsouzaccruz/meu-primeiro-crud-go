945b3d9362732bcd6b2d242f9c9c711cc9195fae
Igor Souza <igorsouzaccruz@gmail.com>
2026-06-07 20:38:09 -0300

    validation user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,22 +2,34 @@ package controller
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/igorsouzaccruz/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/igorsouzaccruz/meu-primeiro-crud-go/src/configuration/validation"
 	"github.com/igorsouzaccruz/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/igorsouzaccruz/meu-primeiro-crud-go/src/controller/model/response"
 )
 
 func CreateUser(c *gin.Context) {
+	log.Println("Init Create User controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadResquestError(
-			fmt.Sprintf("There are some incorrect filds, erros=%s", err.Error()))
+		log.Printf("There are some incorrect filds, erros=%s", err.Error())
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
 	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
